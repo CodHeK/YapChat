33a54dd94d816548d9a86f3ed12d4273852dbd9a
CodHeK <gaganganapathyas@yahoo.in>
2017-06-01 03:11:21 +0530

    Bootstrap Script added to all files

--- chatroom.php
+++ chatroom.php
@@ -211,5 +211,7 @@
   </table>
   </form>
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -62,5 +62,7 @@
     <div class="col-md-4"></div>
     </form>
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
